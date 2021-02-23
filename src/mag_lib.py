dd4f82721648fbf53de2f8f24a3660f83d5fd912
davidkerridge <david41514@gmail.com>
2021-02-23 13:53:17 +0000

    Better calcualtion and printing of field values

--- src/mag_lib.py
+++ src/mag_lib.py
@@ -1,15 +1,28 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Feb  5 12:27:07 2018
+A series of functions designed to help in basic magnetic field data reading
+and manipulation.
 
-Notes: add parameter dtype for d, p, q 
+1. IAGA2002_Header_Reader
+2. IAGA2002_Data_Reader
 
-@author: djk
+Notes for improvements:
+-----------------------
+20 Feb 2021
+-----------
+Might need to add parameter dtype to IAGA Reader to handle different
+INTERMAGNET file types (d=definitive, p=provisional, q=quasi-definitive)
+
+Author
+------
+David Kerridge
+
 """
 
 import os
 import pandas as pd
 import numpy as np
+from collections import namedtuple
 
 r2d = np.rad2deg
 d2r = np.deg2rad
@@ -165,16 +178,16 @@ def load_year(obscode, year, dname):
 def readBmin(obscode, daterange, dname):
     
     """
-    Read in  daily 1-min files between two dates (inclusive)
+    Read in daily 1-min files (IAGA2002 format) over a range of dates
     
     Parameters
     ----------
-        obscode: string
+        obscode: str
             Observatory code e.g. ESK
-        daterange: list, two items, str
+        daterange: list(two items), str
             start date and end date
-        dname: string
-            Top level directory containing the files for the observatory
+        dname: str
+            Name of directory containing the files for the observatory
     
     Returns
     -------
@@ -225,7 +238,8 @@ def read_obs_hmv(obscode, year_st, year_fn, folder):
     """
     OBSY   = obscode.upper()
     obsy   = obscode.lower()
-    # Read in the observatory data one year file at a time and construct filenames
+    # Read in the observatory data one year file at a time and construct 
+    # filenames
     datareq = pd.DataFrame()        
     for year in range(year_st, year_fn+1):
         ystr    = str(year)
@@ -239,15 +253,18 @@ def read_obs_hmv(obscode, year_st, year_fn, folder):
             tmp['Y'] = yvals.round(decimals=1)
         datareq = datareq.append(tmp[['X','Y', 'Z']])
     return(datareq)
-    
+
+#==============================================================================    
 
 def read_obs_hmv_declination(obscode, year_st, year_fn, folder):
-    """Read (or calculate) the declination from hourly mean files in IAGA2002 format.
+    
+    """
+    Read (or calculate) declination from hourly mean files in IAGA2002 format.
 
     This function reads the hourly mean value data in yearly IAGA2002 format
-    files into a pandas dataframe for the specified observatory between year_st
-    and year_fn. Note that D is reported in angular units of minutes of arc (and not
-    degrees) in this file format.
+    files into a pandas dataframe for the specified observatory between 
+    year_st and year_fn. Note that D is reported in angular units of minutes 
+    of arc (and not degrees) in this file format.
 
     Input parameters
     ---------------
@@ -277,7 +294,8 @@ def read_obs_hmv_declination(obscode, year_st, year_fn, folder):
 
     OBSY   = obscode.upper()
     obsy   = obscode.lower()
-    # Read in the observatory data one year file at a time and construct filenames
+    # Read in the observatory data one year file at a time and construct 
+    # filenames
     datareq = pd.DataFrame()
     for year in range(year_st, year_fn+1):
         ystr    = str(year)
@@ -288,7 +306,7 @@ def read_obs_hmv_declination(obscode, year_st, year_fn, folder):
         tmp = tmp.replace(99999.00, np.nan)
         # Calculate D (in degrees) if not given in the file
         if('D' not in tmp.columns):
-            dvals, hvals, ivalsm, fvals  = xyz2dhif(tmp['X'], tmp['Y'], tmp['Z'])
+            dvals, hvals, ivalsm, fvals=xyz2dhif(tmp['X'], tmp['Y'], tmp['Z'])
             tmp.insert(loc=1, column='D', value=dvals.values)
         else:
             # Convert the reported values to degrees
@@ -296,6 +314,7 @@ def read_obs_hmv_declination(obscode, year_st, year_fn, folder):
         datareq = datareq.append(tmp[['D']])
     return(datareq)
 
+#==============================================================================
 
 def read_obs_ann_mean(obscode, filename):
     """Read in the annual mean values for a single observatory.
@@ -330,6 +349,7 @@ def read_obs_ann_mean(obscode, filename):
                 count +=1
     return(df)
 
+#==============================================================================
     
 def obs_ann_means_one_year(year, filename):
     """Read in the annual mean values for all observatories in a given year.
@@ -365,10 +385,12 @@ def obs_ann_means_one_year(year, filename):
                 count +=1
     return(df)
 
+#==============================================================================
    
 def dh2xy(d, h):
-    """Calculate X and Y from D (in 0.1 minutes) and H as reported in IAGA2002
-    format files.
+    """
+    Calculate X and Y from D (in units of 0.1 minutes) and H as reported 
+    in IAGA2002format files.
       
     Input parameters
     ---------------
@@ -391,6 +413,310 @@ def dh2xy(d, h):
     dec = d*np.pi/(180.*60.)
     return((h*np.cos(dec), h*np.sin(dec)))
 
+#==============================================================================
+
+def _mag_el_text():
+    
+    """
+    To give easy access to the conventional labels for the 7 geomagnetic field 
+    elements for use in printed or graphical outputs. The user is assumed to 
+    be aware of the meaning of the abbreviations D, H, Z, X, Y, Z, I, and F.
+    However, this function is intended primarily to be called by other
+    functions.
+    
+    Parameters
+    ----------
+    None
+    
+    Returns
+    -------
+    d: a dictionary with the 7 element abbreviations as keys and each value a 
+    dictionary with 4 keys:
+        name: the full name of the element
+        abbr: the element abbreviation letter in parentheses
+        unit: the unit of measurement
+        svun: the units for secular variation (rate of change)
+        
+    Examples
+    --------
+    >>> _mag_el_text()['I']['name']
+        'Inclination'
+    >>> _mag_el_text()['D']['unit']
+        'deg'
+        
+    Revision date
+    -------------
+    22 Feb 2021
+    
+    """
+    
+    
+    d = {
+        'D': {'name': 'Declination', 'abbr': 'D', 'unit': 'deg', 
+              'svun':'min/y'},
+        'H': {'name': 'Horizontal Intensity', 'abbr': 'H', 'unit': 'nT',
+              'svun': 'nT/y'},
+        'Z': {'name': 'Vertical Intensity', 'abbr': 'Z', 'unit': 'nT',
+              'svun': 'nT/y'},
+        'X': {'name': 'North Component', 'abbr': 'X', 'unit': 'nT',
+              'svun': 'nT/y'},
+        'Y': {'name': 'East Component', 'abbr': 'Y', 'unit': 'nT',
+              'svun': 'nT/y'},
+        'I': {'name': 'Inclination', 'abbr': 'I', 'unit': 'deg', 
+              'svun':'min/y'},
+        'F': {'name': 'Total Intensity', 'abbr': 'F', 'unit': 'nT',
+              'svun': 'nT/y'}
+        }
+            
+    return d
+
+#==============================================================================
+
+def _mag_el_fns(type_='mf'):
+
+    """
+    A collection of lambda functions enabling calculation of geomagnetic
+    field elements from other elements. There are many permutations, and
+    only a few commonly-used conversions are included. The user is assumed to 
+    be aware of the meaning of the abbreviations D, H, Z, X, Y, Z, I, and F.
+    However, this function is intended primarily to be called by other
+    functions.
+    
+    Parameters
+    ----------
+    None
+    
+    Returns
+    -------
+    d: a dictionary with lambda functions as values
+        The keys indicate the functionality. For example key 'F(XYZ)' means
+        the value is a function to compute F from X, Y and Z, which should be 
+        supplied as the input parameters to the lambda function.
+        Input D and I values to the lambda functions are assumed to be in
+        degrees and are output in degrees. Other elements are in units of nT.
+        
+    Dependencies
+    ------------
+    numpy
+    
+    Revision date
+    -------------
+    22 Feb 2021
+    
+    """
+    
+    mf_bank = {
+        'X(HD)'  : lambda h,d: h*np.cos(d2r(d)),
+        'Y(HD)'  : lambda h,d: h*np.sin(np.d2r(d)),
+        'I(ZH)'  : lambda z,h: r2d(np.arctan2(z,h)),
+        'F(HZ)'  : lambda h,z: np.sqrt(h*h+z*z),
+        'D(YX)'  : lambda y,x: r2d(np.arctan2(y,x)),
+        'H(XY)'  : lambda x,y: np.sqrt(x*x+y*y),
+        'I(XYZ)' : lambda x,y,z: r2d(np.arctan2(z, np.sqrt(x*x+y*y))),
+        'F(XYZ)' : lambda x,y,z: np.sqrt(x*x+y*y+z*z),
+        'X(FID)' : lambda f,i,d: f*np.cos(d2r(i))*np.cos(d2r(d)),
+        'Y(FID)' : lambda f,i,d: f*np.cos(d2r(i))*np.sin(d2r(d)),
+        'H(FI)'  : lambda f,i: f*np.sin(d2r(i)),
+        'Z(FI)'  : lambda i,f: f*np.cos(d2r(i))
+        }
+    
+    sv_bank = {
+        'Dd(XYH)' : lambda x,xd,y,yd,h: r2d(60*(xd*y-yd*x)/(h*h)),
+        'Hd(XYH)' : lambda x,xd,y,yd,h: (x*xd+y*yd)/h,
+        'Id(HZF)' : lambda h,hd,z,zd,f: r2d(60*(z*hd-h*zd)/(f*f)),
+        'Fd(XYZF)': lambda x,xd,y,yd,z,zd,f: (x*xd+y*yd+z*zd)/f       
+              }
+    
+    if type_=='mf':
+        return mf_bank
+    elif type_=='sv':    
+        return (mf_bank,sv_bank)
+
+#==============================================================================
+
+def els_calc(xyz):
+    
+    """
+    A function to compute the geomagnetic elements D, H, I and F given X, Y  
+    and Z. (This function could be developed further to accept other input 
+        triplets.)
+    
+    Parameters
+    ----------
+    xyz - tuple, floats
+        X, Y and Z components of a geomagnetic field vector (in nT)
+    
+    Returns
+    -------
+    b: named tuple
+        b contains all 7 elements of the geomagnetic field vector which can be
+        accessed as, for example b.D for declination
+        
+    Dependencies
+    ------------
+    collections
+    _mag_el_fns in this module
+    
+    Revision date
+    -------------
+    22 February 2021
+    
+    """
+    
+    Mag_els = namedtuple('Mag_els', 'X Y Z D H I F')
+    Mag_els.__new__.__defaults__ = (None,)*len(Mag_els._fields)
+    Mag_els.__doc__ = 'Seven elements of the geomagetic field vector'
+
+    b = Mag_els(*xyz)
+    fns = _mag_el_fns()
+    b = b._replace(D = fns['D(YX)'](b.Y,b.X),
+                   H = fns['H(XY)'](b.X,b.Y),
+                   I = fns['I(XYZ)'](b.X, b.Y, b.Z),
+                   F = fns['F(XYZ)'](b.X, b.Y, b.Z)
+                  )
+    return b
+
+#==============================================================================
+
+def els_calc_sv(xyz, xyzdot):
+    
+    """
+    A function to compute the secular variation of geomagnetic elements D, H, 
+    I and F given X, Y, Z and their secuar variation values Xdot, Ydot and
+    Z dot. 
+    
+    Parameters
+    ----------
+    xyz - tuple, floats
+        X, Y and Z components of a geomagnetic field vector (in nT)
+    xyzdot - tuple, floats
+        Xdot, Ydot and Zdot (nT/y)
+    
+    Returns
+    -------
+    sv: named tuple
+        sv contains all 7 elements of the secular variation of the geomagnetic 
+        field vector which can be accessed as, for example sv.Hd for the rate
+        of change of horizontal intensity
+        
+    Dependencies
+    ------------
+    collections
+    _mag_el_fns in this module
+    
+    Revision date
+    -------------
+    22 February 2021
+    
+    """
+
+    Mag_els = namedtuple('Mag_els', 'X Y Z H F')
+    Mag_els.__new__.__defaults__ = (None,)*len(Mag_els._fields)
+    Mag_els.__doc__ = 'Elements required for computing secular variation'
+    
+    SV_els = namedtuple('SV_els', 'Xd Yd Zd Dd Hd Id Fd')
+    SV_els.__new__.__defaults__ = (None,)*len(SV_els._fields)
+    SV_els.__doc__ = 'Seven elements of the secular variation'
+
+    b = Mag_els(*xyz)
+    fns, svfns = _mag_el_fns('sv')
+    b = b._replace(H = fns['H(XY)'](b.X,b.Y),
+                   F = fns['F(XYZ)'](b.X, b.Y, b.Z)
+                  )    
+    
+    sv = SV_els(*xyzdot)
+    sv = sv._replace(Dd=svfns['Dd(XYH)'](b.X, sv.Xd, b.Y, sv.Yd, b.H),
+                     Hd=svfns['Hd(XYH)'](b.X, sv.Xd, b.Y, sv.Yd, b.H),
+                     Fd=svfns['Fd(XYZF)'](b.X, sv.Xd, b.Y, sv.Yd,
+                                          b.Z, sv.Zd, b.F))
+    sv = sv._replace(Id=svfns['Id(HZF)'](b.H, sv.Hd, b.Z, sv.Zd, b.F))
+    
+    return sv
+
+#==============================================================================
+
+def els_prn(b, elstr):
+    
+    """
+    A function to print the geomagnetic elements. It is designed to be used in
+    conjunction with function els_calc in this module
+    
+    Parameters
+    ----------
+    b - named tuple, floats
+            - containing 7 elements of a geomagnetic field vector
+    elstr - str
+        The elements to be printed, in the order in the string
+    
+    Returns
+    -------
+    None
+        
+    Dependencies
+    ------------
+    _mag_el_text in this module
+    
+    Revision date
+    -------------
+    19 Feb 2021
+    
+    """
+
+    e1txt = _mag_el_text()
+    for el in elstr:
+        str1 = e1txt[el]['name']
+        str2 = e1txt[el]['unit']
+        x = b.__getattribute__(el)
+        if el in 'DI':
+            val  = f'{x:.2f}'
+        else:
+            val  = f'{x:.1f}'
+        print( f'{str1:21}: {val:>8} {str2}')
+        
+#==============================================================================
+
+def els_prn_sv(sv, elstr):
+
+    """
+    A function to print the secular variation of the geomagnetic elements. 
+    It is designed to be used in conjunction with function els_calc_sv in 
+    this module
+    
+    Parameters
+    ----------
+    sv - named tuple, floats
+            - containing 7 elements of the rate of change of a geomagnetic
+              field vector
+    elstr - str
+        The elements to be printed, in the order in the string
+    
+    Returns
+    -------
+    None
+        
+    Dependencies
+    ------------
+    _mag_el_text in this module
+    
+    Revision date
+    -------------
+    22 Feb 2021
+    
+    """
+
+    e1txt = _mag_el_text()
+    for el in elstr:
+        idx = el+'d'
+        str1 = e1txt[el]['name']
+        str2 = e1txt[el]['svun']
+        x = sv.__getattribute__(idx)
+        val  = f'{x:.1f}'
+        print( f'{str1:21}: {val:>6} {str2}')
+         
+#====== Some older functions ==================================================
+
+
+#====== els_calc(xyz) is an alternative to xyz2dhif ===========================
 
 def xyz2dhif(x, y, z):
     """Calculate D, H, I and F from (X, Y, Z)
@@ -412,7 +738,7 @@ def xyz2dhif(x, y, z):
     Dependencies
     ------------
     numpy
-         
+          
     Revision date
     -------------
     28 April 2019
@@ -425,24 +751,29 @@ def xyz2dhif(x, y, z):
     inc = np.arctan2(z,hoz)
     return((r2d(dec), hoz, r2d(inc), eff))
 
+#========== els_calc_sv(xyz, xyzdot) is an alternative to xyz2dhif_sv =========
 
 def xyz2dhif_sv(x, y, z, xdot, ydot, zdot):
+    
     """Calculate secular variation in D, H, I and F from (X, Y, Z) and
     (Xdot, Ydot, Zdot)
       
     Input parameters
     ---------------
-    X: north component (nT), and Xdot=dX/dt 
-    Y: east component (nT), and Xdot=dX/dt 
-    Z: vertical component (nT), and Xdot=dX/dt 
+    X: float
+        North component (nT), and Xdot=dX/dt 
+    Y: float
+        East component (nT), and Ydot=dY/dt 
+    Z: float
+        Vertical component (nT), and Zdot=dZ/dt 
     
     Output
     ------
-    A tuple: (Ddot, Hdot, Idot, Fdot)
-    Ddot: rate of change of declination (degrees/year)
-    Hdot: rate of change of horizontal intensity (nT/year)
-    Idot: rate of change of inclination (degrees/year)
-    Fdot: rate of change of total intensity (nT/year)
+    A tuple: (ddot, hdot, idot, fdot)
+        ddot: rate of change of declination (minutes/year)
+        hdot: rate of change of horizontal intensity (nT/year)
+        idot: rate of change of inclination (minutes/year)
+        fdot: rate of change of total intensity (nT/year)
     
     Dependencies
     ------------
@@ -460,88 +791,7 @@ def xyz2dhif_sv(x, y, z, xdot, ydot, zdot):
     fdot = (x*xdot + y*ydot + z*zdot)/np.sqrt(f2)
     ddot = r2d((xdot*y - ydot*x)/h2)*60
     idot = r2d((hdot*z - h*zdot)/f2)*60
-    return((ddot, hdot, idot, fdot))
-#==============================================================================
-"""
-    To give easy access to the conventional labels for the 7 geomagnetic field 
-    elements for use in printed or graphical outputs. The user is assumed to 
-    be aware of the meaning of the abbreviations D, H, Z, X, Y, Z, I, and F.
-    However, this function is intended primarily to be called by other
-    functions.
     
-    Parameters
-    ----------
-    None
-    
-    Returns
-    -------
-    d: a dictionary with the 7 element abbreviations as keys and each value a 
-    dictionary with 3 keys:
-        name: the full name of the element
-        abbr: the element abbreviation letter in parentheses
-        unit: the unit of measurement
-        
-    Examples
-    --------
-    >>> _mag_el_text()['I']['name']
-        'Inclination'
-    >>> _mag_el_text()['D']['unit']
-        'deg'
-"""
-
-def _mag_el_text():
-    
-    d = {
-        'D': {'name': 'Declination', 'abbr': 'D', 'unit': 'deg'},
-        'H': {'name': 'Horizontal Intensity', 'abbr': 'H', 'unit': 'nT'},
-        'Z': {'name': 'Vertical Intensity', 'abbr': 'Z', 'unit': 'nT'},
-        'X': {'name': 'North Component', 'abbr': 'X', 'unit': 'nT'},
-        'Y': {'name': 'East Component', 'abbr': 'Y', 'unit': 'nT'},
-        'I': {'name': 'Inclination', 'abbr': 'I', 'unit': 'deg'},
-        'F': {'name': 'Total Intensity', 'abbr': 'F', 'unit': 'nT'}
-        }
-            
-    return d
-
-#==============================================================================
-"""
-    A collection of lambda functions enabling calculation of geomagnetic
-    field elements from other elements. There are many permutations, and
-    only a few commonly-used conversions are included. The user is assumed to 
-    be aware of the meaning of the abbreviations D, H, Z, X, Y, Z, I, and F.
-    However, this function is intended primarily to be called by other
-    functions.
-    
-    Parameters
-    ----------
-    None
-    
-    Returns
-    -------
-    d: a dictionary with lambda functions as values
-        The keys indicate the functionality. For example key 'F(XYZ)' means
-        the value is a function to compute F from X, Y and Z, which should be 
-        supplied as the inpt parameters to the lambda function.
-        
-    Dependencies
-    ------------
-    numpy
-    
-"""
-
-def _mag_el_fns():
-    
-    fn_bank = {
-        'X(DH)'  : lambda hm,dm: hm*np.cos(d2r(dm)),
-        'Y(DH)'  : lambda hm,dm: hm*np.sin(np.d2r(dm)),
-        'Z(FI)'  : lambda im,fm: fm*np.cos(np.d2r(im)),
-        'D(YX)'  : lambda xm,ym: np.rad2deg(np.arctan2(ym,xm)),
-        'H(XY)'  : lambda xm,ym: np.sqrt(xm*xm+ym*ym),
-        'I(ZH)'  : lambda zm,hm: np.r2d(np.arctan2(zm,hm)),
-        'F(HZ)'  : lambda hm,zm: np.sqrt(hm*hm+zm*zm),
-        'F(XYZ)' : lambda xm,ym,zm: np.sqrt(xm*xm+ym*ym+zm*zm)
-              }
-    
-    return fn_bank
+    return (ddot, hdot, idot, fdot)
 
 #==============================================================================
